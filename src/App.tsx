05375fab8b7bdaeb3f606a05aea85b691213d537
Dustin Johns <dustin.johns1@gmail.com>
2023-06-25 12:38:35 -0500

    initial commit, building react project with vit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Message from './Message';
+
+function App() {
+  const name = 'Dustin';
+  return <div>Message {name}</div>
+}
+
+export default Message;
---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+// Always use PascalCasing or CamelCase in React function names.
+function Message() {
+    return <h1>Hello World</h1>;
+}
+
+export default Message;
